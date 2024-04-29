ad45e195452a0c3e4897e47f816135bede8730c9
Dedlix2104 <Youssef.Ben-Mzoughia@etu.univ-grenoble-alpes.fr>
2024-04-29 16:07:21 +0200

    HELLO JAVA

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+
+import main.Menu.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        Test hi = new Test();
+        hi.printHELLO();
+    }
+}
---
+++ src/main/java/main/Menu/Test.java
@@ -0,0 +1,7 @@
+package main.Menu;
+
+public class Test {
+    public  void printHELLO() {
+        System.out.println("HELLO");
+    }
+}
